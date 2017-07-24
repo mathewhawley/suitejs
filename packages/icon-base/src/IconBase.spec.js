c471196f2997674bd3ff354af5c49625b8ac5ea3
Mathew Hawley <mathew@mathewhawley.com>
2017-07-24 23:24:28 +0100

    feat(icon-base): enable global configuration via context
    The core component can now be configured globally via React's context API. This global configuration
    can be overriden inline via props.

--- packages/icon-base/src/IconBase.spec.js
+++ packages/icon-base/src/IconBase.spec.js
@@ -3,68 +3,68 @@ import { shallow } from 'enzyme';
 import IconBase from './IconBase';
 
 describe('<IconBase />', () => {
-  var wrapper;
-  var defaultProps = {
-    fill: 'currentColor',
-    size: '1em',
-  };
-  var defaultStyle = {
-    verticalAlign: 'text-bottom',
-  };
-
-  beforeEach(() => {
-    wrapper = shallow(
-      <IconBase>
-        <title key="title">an icon</title>
-        <path d="M170 44.1L155.9" key="path" />
-      </IconBase>
-    );
-  });
-
-  it('should correctly set default attributes', () => {
-    var expected = {
-      xmlns: 'http://www.w3.org/2000/svg',
-      fill: defaultProps.fill,
-      height: defaultProps.size,
-      style: defaultStyle,
-      width: defaultProps.size,
-      'aria-hidden': true,
+  describe('settings via props', () => {
+    var wrapper;
+    var defaultAttrs = {
+      fill: 'currentColor',
+      size: '1em',
     };
-    var actual = wrapper.props();
+    var defaultStyle = {
+      verticalAlign: 'text-bottom',
+    };
 
-    Object.keys(expected).forEach(function validateProp(prop) {
-      if (
-        typeof expected[prop] === 'object' &&
-        expected[prop].constructor === Object
-      ) {
-        expect(actual[prop]).toEqual(expected[prop]);
-      } else {
-        expect(actual[prop]).toBe(expected[prop]);
-      }
+    beforeEach(() => {
+      wrapper = shallow(
+        <IconBase>
+          <title>an icon</title>
+          <path d="M170 44.1L155.9" />
+        </IconBase>
+      );
     });
-  });
 
-  describe('customisable attributes', () => {
-    it('fill', () => {
+    it('should correctly set default attributes', () => {
+      var expected = {
+        xmlns: 'http://www.w3.org/2000/svg',
+        fill: defaultAttrs.fill,
+        height: defaultAttrs.size,
+        style: defaultStyle,
+        width: defaultAttrs.size,
+        'aria-hidden': true,
+      };
+      var actual = wrapper.props();
+
+      Object.keys(expected).forEach(function validateProp(prop) {
+        if (
+          typeof expected[prop] === 'object' &&
+          expected[prop].constructor === Object
+        ) {
+          expect(actual[prop]).toEqual(expected[prop]);
+        } else {
+          expect(actual[prop]).toBe(expected[prop]);
+        }
+      });
+    });
+
+    it('should customise fill', () => {
       wrapper.setProps({ fill: '#ffffff' });
       expect(wrapper.prop('fill')).toBe('#ffffff');
     });
 
-    it('width', () => {
+    it('should customise width', () => {
       wrapper.setProps({ width: '1.5em' });
       expect(wrapper.prop('width')).toBe('1.5em');
       wrapper.setProps({ width: 40 });
       expect(wrapper.prop('width')).toBe(40);
     });
 
-    it('height', () => {
+    it('should customise height', () => {
       wrapper.setProps({ height: '2em' });
       expect(wrapper.prop('height')).toBe('2em');
       wrapper.setProps({ height: 24 });
       expect(wrapper.prop('height')).toBe(24);
     });
 
-    it('style', () => {
+    it('should customise style', () => {
       wrapper.setProps({
         style: {
           border: '1px solid #cccccc',
@@ -87,7 +87,7 @@ describe('<IconBase />', () => {
       });
     });
 
-    it('rest', () => {
+    it('should customise rest', () => {
       wrapper.setProps({
         className: 'icon-base',
         'data-icon': true,
@@ -96,6 +96,131 @@ describe('<IconBase />', () => {
       expect(wrapper.prop('className')).toBe('icon-base');
       expect(wrapper.prop('data-icon')).toBe(true);
       expect(wrapper.prop('preserveAspectRatio')).toBe('xMidYMin meet');
+    });
+  });
+
+  describe('settings via context', () => {
+    var wrapper;
+
+    beforeEach(() => {
+      wrapper = shallow(
+        <IconBase>
+          <title>an icon</title>
+          <path d="M170 44.1L155.9" />
+        </IconBase>,
+        { context: {} }
+      );
+    });
+
+    it('should customise fill', () => {
+      wrapper.setContext({
+        iconBase: {
+          fill: '#cccccc',
+        },
+      });
+      expect(wrapper.prop('fill')).toBe('#cccccc');
+
+      // it can be overriden by props
+      wrapper.setProps({
+        fill: '#eeeeee',
+      });
+      expect(wrapper.prop('fill')).toBe('#eeeeee');
+    });
+
+    it('should customise width and height via size', () => {
+      wrapper.setContext({
+        iconBase: {
+          size: '1.5em',
+        },
+      });
+      expect(wrapper.prop('width')).toBe('1.5em');
+      expect(wrapper.prop('height')).toBe('1.5em');
+
+      // it can be overriden by props
+      wrapper.setProps({
+        size: '14px',
+      });
+      expect(wrapper.prop('width')).toBe('14px');
+      expect(wrapper.prop('height')).toBe('14px');
+    });
+
+    it('should customise height', () => {
+      wrapper.setContext({
+        iconBase: {
+          size: '14px',
+          height: 2,
+        },
+      });
+      expect(wrapper.prop('height')).toBe(2);
+
+      // it can be overriden by props
+      wrapper.setProps({
+        size: '12px',
+      });
+      expect(wrapper.prop('height')).toBe('12px');
+    });
+
+    it('should customise width', () => {
+      wrapper.setContext({
+        iconBase: {
+          size: 20,
+          width: '1.15em',
+        },
+      });
+      expect(wrapper.prop('width')).toBe('1.15em');
+
+      // it can be overriden by props
+      wrapper.setProps({
+        size: '10px',
+      });
+      expect(wrapper.prop('width')).toBe('10px');
+    });
+
+    it('should customise style', () => {
+      wrapper.setContext({
+        iconBase: {
+          style: {
+            verticalAlign: 'text-top',
+            backgroundColor: '#cccccc',
+          },
+        },
+      });
+      expect(wrapper.prop('style')).toEqual({
+        verticalAlign: 'text-top',
+        backgroundColor: '#cccccc',
+      });
+
+      // it can be overriden by props
+      wrapper.setProps({
+        style: {
+          backgroundColor: '#000000',
+        },
+      });
+      expect(wrapper.prop('style')).toEqual({
+        verticalAlign: 'text-top',
+        backgroundColor: '#000000',
+      });
+    });
+
+    it('should customise rest', () => {
+      wrapper.setContext({
+        iconBase: {
+          'data-icon': true,
+          className: 'icon',
+          'aria-hidden': false,
+        },
+      });
+      expect(wrapper.prop('data-icon')).toBe(true);
+      expect(wrapper.prop('className')).toBe('icon');
+      expect(wrapper.prop('aria-hidden')).toBe(false);
+
+      // it can be overriden by props
+      wrapper.setProps({
+        className: 'cool-icon',
+        'aria-hidden': true,
+      });
+      expect(wrapper.prop('className')).toBe('cool-icon');
+      expect(wrapper.prop('aria-hidden')).toBe(true);
     });
   });
 });
