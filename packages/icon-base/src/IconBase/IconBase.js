09e9cefc1167ec1513f426d9f69ffb04e385ec3e
Mathew Hawley <mathew@mathewhawley.com>
2017-07-27 19:43:10 +0100

    refactor(icon-base): add documentation to propType definitions

--- packages/icon-base/src/IconBase/IconBase.js
+++ packages/icon-base/src/IconBase/IconBase.js
@@ -4,14 +4,33 @@ import PropTypes from 'prop-types';
 const XMLNS_SVG = 'http://www.w3.org/2000/svg';
 
 var propTypes = {
+  /**
+   * React element(s) passed into the icon
+   */
   children: PropTypes.oneOfType([
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element.isRequired),
   ]).isRequired,
+  /**
+   * Defines the color of the interior
+   */
   fill: PropTypes.string,
+  /**
+   * Height of the root SVG element. Takes precedence over 'size'
+   */
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * Applied to the width and height attributes
+   */
   size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * Added as inline styles to the root SVG element. Shallow merged with
+   * default style object
+   */
   style: PropTypes.object,
+  /**
+   * Width of the root SVG element. Takes precendence over 'size'
+   */
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
