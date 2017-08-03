e2f8b280bd4be768f905a43c9c92d392146e01db
Mathew Hawley <mathew@mathewhawley.com>
2017-08-03 21:40:37 +0100

    refactor(icons): regenerate svgs from sketch
    Regenerate SVG files from Sketch, remove 'removeDimensions' plugin from svgo.json. Add svgo.json
    used by Sketch to project.

--- packages/icons/src/sp/FacebookAlt.js
+++ packages/icons/src/sp/FacebookAlt.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import IconBase from '@suitejs/icon-base';
 
-function FacebookF(props) {
+function FacebookAlt(props) {
   return (
     <IconBase viewBox="0 0 48 48" {...props}>
       <path d="M29.764 11.744c-2.222 0-2.865.988-2.865 3.16v3.605h5.927l-.592 5.829h-5.335v17.68h-7.112v-17.68h-4.79v-5.829h4.79v-3.507c0-5.875 2.371-8.986 8.99-8.986 1.432 0 3.11.098 4.147.246v5.482" />
@@ -9,4 +9,4 @@ function FacebookF(props) {
   );
 }
 
-export default FacebookF;
+export default FacebookAlt;
