174881dab9eb4033c82f47c45528726a6c76a1da
Mathew Hawley <mathew@mathewhawley.com>
2017-08-26 13:14:37 +0100

    chore(icons): fix small errors in scripts

--- packages/icons/scripts/logger.js
+++ packages/icons/scripts/logger.js
@@ -4,6 +4,6 @@ exports.error = function logError(fnName, err) {
   console.error(chalk.white.bgRed(` [ERROR] ${fnName} \n`), err);
 };
 
-exports.success = function logError(msg) {
+exports.success = function logSuccess(msg) {
   console.log(chalk.black.bgGreen(` [DONE] `), msg);
 };
