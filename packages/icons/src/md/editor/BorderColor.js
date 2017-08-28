c9bdd6018d624046c8b70eb403efc03b52a9f064
Mathew Hawley <mathew@mathewhawley.com>
2017-08-28 14:08:14 +0100

    fix(icons): use transparent layers from material design icons

--- packages/icons/src/md/editor/BorderColor.js
+++ packages/icons/src/md/editor/BorderColor.js
@@ -4,7 +4,13 @@ import IconBase from '@suitejs/icon-base';
 function BorderColor(props) {
   return (
     <IconBase viewBox="0 0 48 48" {...props}>
-      <path d="M35.5 13.995l-7.5-7.5-20 20v7.5h7.5l20-20zm5.91-5.91l-3.91 3.91-7.5-7.5 3.91-3.91c.78-.78 2.05-.78 2.83 0l4.67 4.67c.78.78.78 2.05 0 2.83zM0 39.995h48v8H0v-8z" />
+      <g>
+        <path d="M35.5 13.995l-7.5-7.5-20 20v7.5h7.5l20-20zm5.91-5.91c.78-.78.78-2.05 0-2.83L36.74.585c-.78-.78-2.05-.78-2.83 0L30 4.495l7.5 7.5 3.91-3.91z" />
+        <polygon
+          fillOpacity=".36"
+          points="0 39.995 48 39.995 48 47.995 0 47.995"
+        />
+      </g>
     </IconBase>
   );
 }
--- packages/icons/src/md/editor/FormatColorText.js
+++ packages/icons/src/md/editor/FormatColorText.js
@@ -4,7 +4,10 @@ import IconBase from '@suitejs/icon-base';
 function FormatColorText(props) {
   return (
     <IconBase viewBox="0 0 48 48" {...props}>
-      <path d="M22 3h4l11 28h-4.5l-2.25-6h-12.5l-2.25 6H11L22 3zm-2.75 18h9.5L24 8.33 19.25 21zM0 37h48v8H0v-8z" />
+      <g>
+        <polygon fillOpacity=".36" points="0 37 48 37 48 45 0 45" />
+        <path d="M22 3L11 31h4.5l2.25-6h12.5l2.25 6H37L26 3h-4zm-2.75 18L24 8.33 28.75 21h-9.5z" />
+      </g>
     </IconBase>
   );
 }
